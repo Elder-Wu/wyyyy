856b514b1ac4afae6f343a196ea1b0179a3a1004
wuming <itiswm@163.com>
2016-11-21 10:23:06 +0800

    第一次提交，测试仓库的连接状态

---
+++ app/src/main/java/com/wuzhanglao/wyyyy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wuzhanglao.wyyyy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
